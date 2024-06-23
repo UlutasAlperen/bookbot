d33507d2f4ded1ef1577d4a44c2ee1c5a06887ea
Alapt <ulutas.alperen@outlook.com>
2024-06-23 18:11:54 +0300

    rewrite main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def main():
+    path ="books/frankenstein.txt"
+    print(get_the_text(path))
+
+
+def get_the_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
