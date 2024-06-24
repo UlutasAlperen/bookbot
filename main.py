692af231de33644ee9b4df5ea860341a36a40f97
Alapt <ulutas.alperen@outlook.com>
2024-06-24 17:08:59 +0300

    add create_dict func

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     path ="books/frankenstein.txt"
-    print(get_the_text(path))
+    print(create_dict(get_the_text(path)))
 
 
 def get_the_text(path):
     with open(path) as f:
         return f.read()
 
+def create_dict(text):
+    text = list(str(text).lower()) 
+    my_dict ={}
+    my_set = set(text)
+    for i in my_set:
+        my_dict[i]=text.count(i)
+    return my_dict
 
 main()
